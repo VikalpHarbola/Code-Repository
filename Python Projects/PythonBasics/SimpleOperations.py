3b098bfe048e098b99db20181480a3df0f739202
VikalpHarbola <vharbola@gmail.com>
2019-08-23 05:57:21 +0530

    Python Simple Operations Program

---
+++ Python Projects/PythonBasics/SimpleOperations.py
@@ -0,0 +1,31 @@
+#This File provides overview to simple operations which can be performed in Python
+print('Direct addition',2+2)
+var1 =2
+var2 = 2
+var3= var1+var2
+print ('Variable Addition',var1+var2)
+print('Final Variable Print',var3)
+#############################################
+var1=10
+var2=5
+var3=6
+var_print=var1*(var2+var3)
+print('var_print',var_print)
+var_print=var1/var2
+print('var_print',var_print)
+var_print=var2*(4+var3/var1)
+print('var_print',var_print)
+###################################################
+####Negative Number Calculations#################
+var1=-10
+var2=-5
+var3=-6
+var_print=var1*(var2+var3)
+print('var_print',var_print)
+###############################################
+###########Error Messages#####################
+var1=-10
+var2=-5
+var3=5
+var_print=var1/(var2+var3)
+print('var_print',var_print)
