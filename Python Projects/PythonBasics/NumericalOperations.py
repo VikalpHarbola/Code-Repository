810701c6af9c1b62fce611c8c0b74450d0871c7e
VikalpHarbola <vharbola@gmail.com>
2019-08-23 06:27:13 +0530

    Python Numerical Operation Program

---
+++ Python Projects/PythonBasics/NumericalOperations.py
@@ -0,0 +1,27 @@
+#Floats are those numerical vales which also hold decimal values post decimal places
+Var=3/4
+print('Var',Var)
+
+print(8/2)
+
+print(6*43.0)
+
+print(4+1.15)
+
+##Exponentiation Raising value of a to the power of n
+a=2
+n=3
+value=a**n
+print('Exponentiation Values',value)
+a=2
+n=1/2
+value=a**n
+print('Exponentiation Values',value)
+
+#### Quotient & Remainder
+a=7
+n=2
+value=a//n
+print('Quotient Values',value)
+value=a%n
+print('Remainder Values',value)
